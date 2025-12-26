c04544373cac06036e77ebcfe5c37d83b53591f2
OmarNjadallah <jadallahomar22@gmail.com>
2025-12-26 17:24:05 +0300

    collision detection

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
-from logger import log_state
+from logger import log_state,log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroidfeild = AsteroidField()
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,13 +26,21 @@ def main():
                 return
         dt = clock.tick(60) / 1000
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
+        for sprite in asteroids:
+            if(sprite.collides_with(player)):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
        
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
+    print(len(asteroids))
+
 
 
 if __name__ == "__main__":
